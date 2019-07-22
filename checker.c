fa462c1720179f9d7c07ab8704ff54592ae72a97
Gabriel Strauss <gstrauss@c5r5s4.wethinkcode.co.za>
2019-07-22 14:10:56 +0200

    stuff

--- checker.c
+++ checker.c
@@ -6,57 +6,83 @@
 /*   By: gstrauss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/19 09:47:38 by gstrauss          #+#    #+#             */
-/*   Updated: 2019/07/22 09:22:48 by gstrauss         ###   ########.fr       */
+/*   Updated: 2019/07/22 14:08:02 by gstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <stdio.h>
 
+void	check(char *line, t_list *lista, t_list *listb);
+void	checks(char *line, t_list *lista, t_list *listb);
+
 int main(int argc, char **argv)
 {
 	char *line;
-
+	
+	t_list *lista;
+	t_list *listb = NULL;
+	lista = ft_lstmake(argv, ' ', 1);
 	while(get_next_line(1, &line))
 	{
-		printf("%s\n", line);
+		if(argc > 1)
+		{
+			check(line, lista, listb);
+			while(lista)
+			{
+				printf("lista: %s\n", lista->content);
+				if(lista->next)
+					lista = lista->next;
+				else
+					break;
+			}
+			/*if(listb->content)
+			{
+				printf("\n");
+				printf("listb: %s\n", listb->content);
+			}*/
+		}
 		free(line);
 	}
-	if(argc > 1)
-	{
-		if(ft_strcmp(argv[1], "sa") == 0)
-		{
-		}
-		if(ft_strcmp(argv[1], "sb") == 0)
-		{
-		}
-		if(ft_strcmp(argv[1], "ss") == 0)
-		{
-		}
-		if(ft_strcmp(argv[1], "pa") == 0)
-		{
-		}
-		if(ft_strcmp(argv[1], "pb") == 0)
-		{
-		}
-		if(ft_strcmp(argv[1], "ra") == 0)
-		{
-		}
-		if(ft_strcmp(argv[1], "rb") == 0)
-		{
-		}
-		if(ft_strcmp(argv[1], "rr") == 0)
-		{
-		}
-		if(ft_strcmp(argv[1], "rra") == 0)
-		{
-		}
-		if(ft_strcmp(argv[1], "rrb") == 0)
-		{
-		}
-		if(ft_strcmp(argv[1], "rrr") == 0)
-		{
-		}
-	}
 	return(0);
+}
+
+void	check(char *line, t_list *lista, t_list *listb)
+{
+		if(ft_strcmp(line, "sa") == 0)
+			sa(lista);
+		if(ft_strcmp(line, "sb") == 0)
+			sb(listb);
+		if(ft_strcmp(line, "ss") == 0)
+		{
+			sa(lista);
+			sb(listb);
+		}
+		if(ft_strcmp(line, "pa") == 0)
+			pa(&lista, &listb);
+		if(ft_strcmp(line, "pb") == 0)
+			pb(&lista, &listb);
+		if(ft_strcmp(line, "ra") == 0)
+			ra(&lista);
+		if(ft_strcmp(line, "rb") == 0)
+			rb(&listb);
+		checks(line, lista, listb);
+}
+
+void	checks(char *line, t_list *lista, t_list *listb)
+{
+		if(ft_strcmp(line, "rr") == 0)
+		{
+			ra(&lista);
+			rb(&listb);
+		}
+		if(ft_strcmp(line, "rra") == 0)
+			rra(&lista);
+		if(ft_strcmp(line, "rrb") == 0)
+			rrb(&listb);
+		if(ft_strcmp(line, "rrr") == 0)
+		{
+			rra(&lista);
+			rrb(&listb);
+		}
 }
